e31d5e12fa6c4c3f02d6cb97cc4b243bfc06b478
Andriy_Kuzmych <75886738+Andrew285@users.noreply.github.com>
2021-11-16 16:17:49 +0200

    Add files via upload

--- client_lib.py
+++ client_lib.py
@@ -1,13 +1,16 @@
 import psycopg2  # postgreSQL package
 from data import DataAPI
 from managment import ManagmentAPI
+from flask import Flask, redirect, url_for, request
+
+app = Flask(__name__)
 
 
 class Client:
-    def __init__(self, mnode_address):
-        self.mnode_address = mnode_address  # ip address/ For example {"127.0.0.1:8011"}
+    def __init__(self, node_address):
+        self.node_address = node_address  # ip address/ For example {"127.0.0.1:8011"}
         self.stats = None  # statistics
-        self.M_API = ManagmentAPI(mnode_address)  # management node
+        self.M_API = ManagmentAPI(node_address)  # management node
         self.W_API = None  # data node with minimal amount of messages
         self.R_API = None  # data node with maximum amount of messages
         self.counter = 0
@@ -23,11 +26,42 @@ class Client:
         self.counter = (self.counter + 1) % 100
 
     # add message to the database
-    def push_message(self, mssg):
+    def push_message(self):
         self._update_stats()
-        self.W_API.write_message(mssg)
+
+        @app.route('/success/<name>')
+        def success(name):
+            return 'Message: %s added' % name
+
+        @app.route('/login', methods=['POST', 'GET'])
+        def login():
+            if request.method == 'POST':
+                text = request.form['nm']
+                self.W_API.write_message(text)
+                return redirect(url_for('success', name=text))
+            else:
+                text = request.args.get('nm')
+                return redirect(url_for('success', name=text))
 
     # get message from the database
     def get_message(self):
         self._update_stats()
-        return self.R_API.read_message()
+
+        @app.route('/get_mssg')
+        def extra():
+            return self.R_API.read_message()
+
+
+
+client = Client('127.0.0.1:8011')
+
+ip_address = "127.0.0.1"
+port = 8011
+text = "text_"
+
+
+# client.get_message()
+# client.push_message()
+app.run()
+
+
